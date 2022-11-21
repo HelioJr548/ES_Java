6f369f97ac7d0f47152479e2949a7c2af89ac416
Helio de Jesus dos Santos Junior <84392613+HelioJr548@users.noreply.github.com>
2022-11-21 09:07:36 -0300

    Listas Encadeadas

---
+++ Aula11-Listas_Encadeadas/src/Exemplo/Main.java
@@ -0,0 +1,14 @@
+package Exemplo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Lista contato = new Lista();
+		contato.adicionar(new Contato("Ana", "ana@gmail.com"));
+		contato.adicionar(new Contato("Paulo", "pa@gmail.com"));
+		contato.toString();
+		
+	}
+
+}
